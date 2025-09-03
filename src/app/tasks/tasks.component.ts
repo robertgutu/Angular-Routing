a66649e859e7470c12596e8e1924fe0bde649db2
robertgutu <gutu.robert49@yahoo.com>
2025-09-03 15:24:28 +0300

    Extracting Query Parameters via Inputs

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -14,6 +14,7 @@ import { RouterLink } from "@angular/router";
 })
 export class TasksComponent {
   userId = input.required<string>()
+  order = input<'asc' | 'desc'>()
   // userTasks: Task[] = [];
   private tasksService = inject(TasksService)
   userTasks = computed(() =>{
