c11a8b3babf0eb42563176e9a5f2804d32931c18
robertgutu <gutu.robert49@yahoo.com>
2025-09-03 14:20:44 +0300

    Route links and relative links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from "@angular/router";
 
-// import { TaskComponent } from "./tasks/task/task.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 export const routes:Routes = [
         {
@@ -11,6 +12,16 @@ export const routes:Routes = [
         },
         {
             path: 'users/:userId',
-            component: UserTasksComponent
+            component: UserTasksComponent,
+            children: [
+                {
+                    path: 'tasks',
+                    component: TasksComponent
+                },
+                {
+                    path: 'tasks/new',
+                    component: NewTaskComponent
+                }
+            ]
         }
     ]
